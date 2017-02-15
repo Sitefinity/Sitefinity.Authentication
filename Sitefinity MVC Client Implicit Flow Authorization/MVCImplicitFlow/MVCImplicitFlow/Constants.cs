c79d4de466f5836806199dd40c5898735e41d75b
Aleksandra Stoycheva <astojceva@yahoo.com>
2017-02-15 11:30:15 +0200

    Fix constant SitefinityBaseUrl

--- Sitefinity MVC Client Implicit Flow Authorization/MVCImplicitFlow/MVCImplicitFlow/Constants.cs
+++ Sitefinity MVC Client Implicit Flow Authorization/MVCImplicitFlow/MVCImplicitFlow/Constants.cs
@@ -7,8 +7,8 @@ namespace MVCImplicitFlow
 {
     public class Constants
     {
-        public const string SitefinityBaseUrl = "http://localhost:1010";
-        public const string SitefinitySTSUrl = SitefinityBaseUrl + "/Sitefinity/Authenticate/OpenID";
+        public const string SitefinityBaseUrl = "http://localhost:1010/";
+        public const string SitefinitySTSUrl = SitefinityBaseUrl + "Sitefinity/Authenticate/OpenID";
         public const string SiteUrl = "http://localhost:64761/";
         public const string SitefinityNewsItemsApiUrl = SitefinityBaseUrl + "api/default/newsitems";
         public const string ClientId = "testApp";
